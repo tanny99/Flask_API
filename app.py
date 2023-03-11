939bfa056c20a469e080bd04d1441a83a6d05897
Tanishk gupta <55141974+tanny99@users.noreply.github.com>
2023-03-12 01:39:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello():
-    return jsonify({'message': 'Hello, world!'})
+@app.route('/api/data', methods=['POST'])
+def post_data():
+    input_data = request.form.get('input')
+    # Code for processing the input data
+    result = {"success": True, "data": input_data}
+    return jsonify(result)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+ 
+# from flask import Flask, jsonify
+
+# app = Flask(__name__)
+
+# @app.route('/hello')
+# def hello():
+#     return jsonify({'message': 'Hello, world!'})
+
+# if __name__ == '__main__':
+#     app.run()
