2b3a9361900467a6d85687747ba114ffc17f644b
Tanishk gupta <55141974+tanny99@users.noreply.github.com>
2023-03-12 02:47:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from flask import Flask, jsonify, request
-
 app = Flask(__name__)
+from googletrans import Translator
+
+# create a translator object
+translator = Translator()
+
+# translate from English to French
+
+
+# print the translated text
+# print(french_translation.text)
 
 @app.route('/api/data', methods=['POST'])
 def post_data():
     input_data = request.form.get('input')
     # Code for processing the input data
-    result = {"success": True, "data": input_data}
+#     english_text = "Hello, how are you?"
+    french_translation = translator.translate(input_data, dest='fr')
+    result = {"success": True, "data": french_translation}
     return jsonify(result)
 
 if __name__ == '__main__':
