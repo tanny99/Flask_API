be4b6301b93fb805f5ba572cd2fcaecd6a4767d9
Ujjwal Sachdeva <ujjwal.sachdeva24@gmail.com>
2023-04-25 14:36:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
-allowed_origins = ['*', 'https://delightful-creponne-c38588.netlify.app']
+allowed_origins = ['*', 'https://36majorproject.netlify.app']
 @app.before_request
 def before_request():
     origin = request.headers.get('Origin')
